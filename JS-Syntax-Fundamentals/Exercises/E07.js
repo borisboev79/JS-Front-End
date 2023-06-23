7956b660b6c06631e1a9b721d733bee2e055b5cc
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2023-06-23 03:02:58 +0300

    Add files via upload

---
+++ JS-Syntax-Fundamentals/Exercises/E07.js
@@ -0,0 +1,23 @@
+function toString(char1, char2, char3) {
+// Varian 2
+//     let arr = [char1, char2, char3];
+//     console.log(arr.join(''));
+
+
+// Variant 2
+
+let text = '';
+
+text+= char1;
+text+= char2;
+text+= char3;
+
+console.log(text)
+
+
+}
+
+toString('%',
+'2',
+'o'
+)
