7fee9b719c1522a3c4e6470835a53ed5f672b98a
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2023-06-23 19:32:46 +0300

    Add files via upload

---
+++ JS-Syntax-Fundamentals/Lab/11.js
@@ -0,0 +1,9 @@
+function sumFirstAndLastElement(arr) {
+    const first = arr[0];
+    const last = arr[arr.length-1];
+
+    console.log(first + last)
+}
+
+sumFirstAndLastElement([10, 17, 22, 33]);
+
