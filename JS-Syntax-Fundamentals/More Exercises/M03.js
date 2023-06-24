482ef2644f62ee17cc2d480d1e8b27e88a97e23d
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2023-06-24 18:22:31 +0300

    Add files via upload

---
+++ JS-Syntax-Fundamentals/More Exercises/M03.js
@@ -0,0 +1,11 @@
+function calculate(num1, operator, num2) {
+    let result = 0;
+    switch(operator) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+    }
+
+    console.log(result.toFixed(2));
+}
