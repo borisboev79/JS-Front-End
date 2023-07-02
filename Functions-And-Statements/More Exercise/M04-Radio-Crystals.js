0acf623fedb359951acd02bfd990e54484838ad8
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2023-07-02 14:05:32 +0300

    Add files via upload

---
+++ Functions-And-Statements/More Exercise/M04-Radio-Crystals.js
@@ -0,0 +1,69 @@
+function refineCrystal(input) {
+  const [target, ...chunks] = input;
+
+  const canCut = (chunk) => chunk / 4 >= target;
+  const cut = (chunk) => chunk / 4;
+  const lap = (chunk) => chunk - chunk * 0.2;
+  const grind = (chunk) => chunk - 20;
+  const etch = (chunk) => chunk - 2;
+  const xRay = (chunk) => chunk + 1;
+
+  const operations = [lap, grind, etch];
+
+  for (let chunk of chunks) {
+    console.log(`Processing chunk ${chunk} microns`);
+
+    while (chunk > target) {
+      // Cut...
+      if (canCut(chunk)) {
+        let counter = 0;
+
+        while (canCut(chunk)) {
+          chunk = cut(chunk);
+          counter++;
+        }
+
+        console.log(`Cut x${counter}`);
+        chunk = washChunk(chunk);
+      }
+
+      //Lap, Grind, Etch...
+      for (const operation of operations) {
+        if (operation(chunk) >= target) {
+          let counter = 0;
+
+          while (
+            operation !== etch
+              ? operation(chunk) >= target
+              : operation(chunk) >= target - 1
+          ) {
+            chunk = operation(chunk);
+            counter++;
+          }
+
+          let capitalized =
+            operation.name.charAt(0).toUpperCase() + operation.name.slice(1);
+
+          console.log(`${capitalized} x${counter}`);
+          chunk = washChunk(chunk);
+        }
+      }
+    }
+
+    //X-ray...
+
+    if (xRay(chunk) === target) {
+      chunk = xRay(chunk);
+      console.log("X-ray x1");
+    }
+
+    console.log(`Finished crystal ${target} microns`);
+  }
+
+  function washChunk(chunk) {
+    console.log("Transporting and washing");
+    return Math.floor(chunk);
+  }
+}
+
+refineCrystal([1375, 50000]);
