8394c104cb5816fc2d8e187664b1d70114ab4713
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2023-07-05 22:12:23 +0300

    Add files via upload

---
+++ Objects-And-Classes/Lab/02-City.js
@@ -0,0 +1,13 @@
+function displayCity(city) {
+  Object.entries(city).forEach((pair) => {
+    console.log(`${pair[0]} -> ${pair[1]}`);
+  });
+}
+
+displayCity({
+  name: "Sofia",
+  area: 492,
+  population: 1238438,
+  country: "Bulgaria",
+  postCode: "1000",
+});
