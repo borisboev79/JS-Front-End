4287ff321264155d94cc57c71e1141e8fdc5a860
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2023-07-05 20:43:10 +0300

    Create 01-Person-Info.js

---
+++ Objects-And-Classes/Lab/01-Person-Info.js
@@ -0,0 +1,9 @@
+function createPerson(firstName, lastName, age) {
+  const person = {};
+  person.firstName = firstName;
+  person.lastName = lastName;
+  person.age = age;
+  return person;
+}
+
+console.log(createPerson("Peter", "Pan", "20"));
