c582dd2adee4ac69a42358ad0b013a447b728334
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2023-07-07 00:28:43 +0300

    Add files via upload

---
+++ Objects-And-Classes/Lab/07-Meetings.js
@@ -0,0 +1,26 @@
+function scheduleMeeting(arr){
+    const schedule = arr.reduce((acc, curr) => {
+        const [day, name] = curr.split(' ');
+         
+         if(acc.hasOwnProperty(day)){
+            console.log(`Conflict on ${day}!`)
+         } else {
+                acc[day] = name;
+                console.log(`Scheduled for ${day}`)
+         }
+
+         return acc;
+    }, {})
+
+     Object.keys(schedule).forEach(key => {
+        console.log(key + ' -> ' + schedule[key])
+     })
+    
+    };
+
+
+scheduleMeeting(['Friday Bob',
+'Saturday Ted',
+'Monday Bill',
+'Monday John',
+'Wednesday George'])
